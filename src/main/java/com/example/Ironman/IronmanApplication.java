95a25547fc72f3830b710b7dd2da0f66a96ffa40
devipamu <devipamu34592@gmail.com>
2023-12-06 12:02:58 +0530

    first commit

---
+++ src/main/java/com/example/Ironman/IronmanApplication.java
@@ -0,0 +1,14 @@
+package com.example.Ironman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IronmanApplication.class, args);
+		System.out.println("I like Ironman");
+	}
+
+}
---
+++ src/test/java/com/example/Ironman/IronmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ironman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IronmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
